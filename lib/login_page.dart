ee9c6672a5595912467e757699a62662db043c6e
jarin <jarin@polygontech.xyz>
2024-05-05 17:36:16 +0600

    button style

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -6,83 +6,160 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.blue[900],
-        body: Column(
-          children: [
-            Text(
-              'Hello, welcome back!',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
+      backgroundColor: Colors.blue[900],
+      body: Column(
+        children: [
+          Text(
+            'Hello, welcome back!',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(
+            height: 32,
+            width: 50,
+            // child: Text('text for sizebox'),
+          ),
+          Text(
+            'Login to continue',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          TextField(
+            decoration: InputDecoration(
+              hintText: 'Username',
+              filled: true,
+              fillColor: Colors.white.withOpacity(0.3),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
               ),
             ),
-            SizedBox(
-              height: 32,
-              width: 50,
-              // child: Text('text for sizebox'),
-            ),
-            Text(
-              'Login to continue',
-              style: TextStyle(
-                color: Colors.white,
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          TextField(
+            decoration: InputDecoration(
+              hintText: 'Password',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
               ),
+              filled: true,
+              fillColor: Colors.white.withOpacity(0.3),
             ),
-            TextField(
-              decoration: InputDecoration(
-                hintText: 'Username',
-                border: InputBorder.none,
-                filled: true,
-                fillColor: Colors.white.withOpacity(0.3),
-              ),
-            ),
-            TextField(decoration: InputDecoration(hintText: 'Password')),
-            TextButton(
+          ),
+          Align(
+            alignment: Alignment.centerRight,
+            child: TextButton(
                 onPressed: () {
                   print('forgot is Clicked');
                 },
-                child: Text('Forgot Password Button')),
-            ElevatedButton(
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.white,
+                ),
+                child: Text('Forgot Password?')),
+          ),
+          SizedBox(
+            width: 250,
+            child: ElevatedButton(
                 onPressed: () {
                   print('login is Clicked');
                 },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.amber,
+                  foregroundColor: Colors.black,
+                ),
                 child: Text('Log in')),
-            Text('Or sign in with'),
-            ElevatedButton(
-                onPressed: () {
-                  print('google is Clicked');
-                },
-                child: Row(
-                  children: [
-                    Image.asset(
-                      'assets/images/google.jpeg',
-                      width: 22,
-                      height: 22,
-                    ),
-                    Text('Login with Google'),
-                  ],
-                )),
-            ElevatedButton(
-                onPressed: () {
-                  print('Facebook is Clicked');
-                },
-                child: Row(
-                  children: [
-                    Image.asset(
-                      'assets/images/facebook.png',
-                      width: 22,
-                      height: 22,
-                    ),
-                    Text('Login with Facebook'),
-                  ],
-                )),
-            Row(
-              children: [
-                Text("Don't have account?"),
-                TextButton(onPressed: () {}, child: Text('Sign up')),
-              ],
-            )
-          ],
-        ));
+          ),
+          SizedBox(
+            height: 62,
+          ),
+          Text(
+            'Or sign in with',
+            style: TextStyle(color: Colors.white),
+          ),
+          ElevatedButton(
+              onPressed: () {
+                print('google is Clicked');
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                foregroundColor: Colors.black,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(50),
+                  ),
+                ),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/images/google.jpeg',
+                    width: 22,
+                    height: 22,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text('Login with Google'),
+                ],
+              )),
+          ElevatedButton(
+              onPressed: () {
+                print('Facebook is Clicked');
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                foregroundColor: Colors.black,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(50),
+                  ),
+                ),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/images/facebook.png',
+                    width: 22,
+                    height: 22,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text('Login with Facebook'),
+                ],
+              )),
+          Row(
+            children: [
+              Text(
+                "Don't have account?",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.amber,
+                ),
+                child: Text(
+                  'Sign up',
+                  style: TextStyle(decoration: TextDecoration.underline),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
